a913dd7bb3691c6b4c709d2c7f40fdb645c5fffc
2dubu <lgw101142@gmail.com>
2026-05-06 15:30:13 +0900

    Add Force Quit Process command UI

---
+++ src/process.tsx
@@ -0,0 +1,47 @@
+import { Action, ActionPanel, Icon, List } from "@raycast/api";
+import { useCachedPromise } from "@raycast/utils";
+import { fetchAllProcesses } from "./lib/processes";
+import { killByPid } from "./lib/kill";
+import { formatMemoryMB } from "./lib/format";
+
+export default function Command() {
+  const { data, isLoading, revalidate } = useCachedPromise(fetchAllProcesses, [], { initialData: [] });
+
+  return (
+    <List isLoading={isLoading} searchBarPlaceholder="Search processes...">
+      <List.EmptyView title="No processes found" />
+      {data.map((proc) => (
+        <List.Item
+          key={proc.pid}
+          icon={Icon.Terminal}
+          title={proc.name}
+          subtitle={`PID ${proc.pid}`}
+          accessories={[{ text: formatMemoryMB(proc.memoryMB) }]}
+          actions={
+            <ActionPanel>
+              <Action
+                title="Force Quit"
+                icon={Icon.XMarkCircle}
+                onAction={async () => {
+                  const ok = await killByPid(proc.pid, proc.name);
+                  if (ok) revalidate();
+                }}
+              />
+              <Action
+                title="Refresh List"
+                icon={Icon.ArrowClockwise}
+                shortcut={{ modifiers: ["cmd"], key: "r" }}
+                onAction={revalidate}
+              />
+              <Action.CopyToClipboard
+                title="Copy Process Info"
+                content={`${proc.name} (PID ${proc.pid})`}
+                shortcut={{ modifiers: ["cmd"], key: "." }}
+              />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
+}
